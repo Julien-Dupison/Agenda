3282ea9faa7c62ce6b5dfc28e334fa3bac0a94f2
infiniityr <tintionu242>
2017-02-06 13:58:53 +0100

    gitignore update

--- database.php
+++ database.php
@@ -1,11 +1,20 @@
 <?php
-include "params.php";
+try {
+    include "params.php";
+}
+catch (Exception $e)
+{
+    //Le fichier n'existe pas
+    file_put_contents("params.php", file_get_contents("paramsExemple.php"));
+    include "params.php";
+}
+
+
 try
 {
 	$db = new PDO('mysql:host='.BD_HOST.':'.BD_PORT.';dbname='.BD_NAME.'', BD_USER, BD_PASSWORD);
 	$db->exec("SET CHARACTER SET utf8");
 }
-
 catch (PDOException $e)
 {
     print "Erreur !: " . $e->getMessage() . "<br/>";
---
+++ paramsExemple.php
@@ -0,0 +1,22 @@
+<?php
+
+//Base de données
+
+define("BD_HOST", "localhost");
+define("BD_PORT", "3006");
+define("BD_NAME", "bd");
+define("BD_USER", "root");
+define("BD_PASSWORD", "");
+
+//Serveur 3IL
+define("URL_EDT", "https://exnet.3il.fr/");
+define("USER_TEST", "");
+define("PWD_TEST", "");
+
+//Infos serveur
+define("PATH", "/path/to/project/Agenda");
+define("ADDRESS", "http://localhost:8000/");
+define("IP_ADDRESS", "127.0.0.1:8000");
+define("LOG_PATH", "sauv/logs/")
+
+?>
